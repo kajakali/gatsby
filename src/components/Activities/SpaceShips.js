64f42149b16c8bb2a5d7b896ed857bf54c7cae8f
Frieda Jacobson <kajakali@hotmail.com>
2020-08-07 21:22:00 -0500

    created a form for the ship selection

--- src/components/Activities/SpaceShips.js
+++ src/components/Activities/SpaceShips.js
@@ -3,6 +3,11 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import TheShip from './TheShip';
 import Box from '@material-ui/core/Box';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 
 class SpaceShips extends Component {
@@ -33,6 +38,18 @@ class SpaceShips extends Component {
             ?
             <>
             <TextField />
+            <FormControl component="fieldset">
+      <FormLabel component="legend">Style</FormLabel>
+        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+            {this.state.possibleShips.map((item, index) => (
+                <FormControlLabel value={index} control=<Radio /> label={item} />
+            ) )}
+            <FormControlLabel value="0" control={<Radio />} label="Female" />
+            <FormControlLabel value="1" control={<Radio />} label="Male" />
+            <FormControlLabel value="2" control={<Radio />} label="Other" />
+            <FormControlLabel value="3" control={<Radio />} label="(Disabled option)" />
+        </RadioGroup>
+      </FormControl>
             <Button color="primary" variant="contained" size="small" onClick={this.finishSpaceship}>Finish the Spaceship!</Button>
             </>
             :
